a3f2fcab9a49a845c124c8543cad84468a4e5bc5
panavrin <sangwonlee@0587364789.wireless.umich.net>
2015-11-24 11:21:59 -0500

    dialog jQuery added

--- crowds_project_deploy_folder/client/js/crowds_project.js
+++ crowds_project_deploy_folder/client/js/crowds_project.js
@@ -1,3 +1,6 @@
+var DEBUG = true;
+
+//version = Meteor.collection("version_number")
 
 if (Meteor.isClient) {
   // counter starts at 0
@@ -15,17 +18,23 @@ if (Meteor.isClient) {
     }
   });
   */  
-  Session.setDefault('document', "javascriptDoc");
+  Session.setDefault('document', "collab_python_doc");
 
+  Template.cm_task_view.onRendered(function () {
+  // Use the Packery jQuery plugin
+    console.log("task_on_rendered");
+    this.$('.task-wrapper').dialog();
+  });
 
   Template.cm_code_editor.helpers({
     config: function() {
       return function(cm) {
+        if (DEBUG) {console.log("config function running");}
         cm.setOption("theme", "default");
         cm.setOption("lineNumbers", true);
         cm.setOption("lineWrapping", true);
         cm.setOption("smartIndent", true);
-
+        cm.setOption("mode", "text/x-python");
         return cm.setOption("indentWithTabs", true);
       };
     },
